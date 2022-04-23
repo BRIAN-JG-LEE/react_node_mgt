cc7541d9ad0bc0ad2bc7de1e31407a7fb02c35d3
JONGGIL LEE <brianlee2k2@gmail.com>
2022-04-24 08:57:38 +0900

    5. 고객 컴포넌트 구조화하기 / map 함수를 사용하여 반복된 값을 출력할 땐 key= 라는 이름의 props 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "김철수",
-  birthday: "901231",
-  gender: "남",
-  job: "디자이너",
-};
+const customers = [
+  {
+    id: 123,
+    image: "https://placeimg.com/128/128/1",
+    name: "김철수",
+    birthday: "901231",
+    gender: "남",
+    job: "디자이너",
+  },
+  {
+    id: 456,
+    image: "https://placeimg.com/128/128/2",
+    name: "나영희",
+    birthday: "941130",
+    gender: "여",
+    job: "기획자",
+  },
+  {
+    id: 789,
+    image: "https://placeimg.com/128/128/3",
+    name: "이종길",
+    birthday: "910101",
+    gender: "남",
+    job: "프로그래머",
+  },
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {customers.map((cus) => {
+        return (
+          <Customer
+            key={cus.id}
+            id={cus.id}
+            image={cus.image}
+            name={cus.name}
+            birthday={cus.birthday}
+            gender={cus.gender}
+            job={cus.job}
+          />
+        );
+      })}
+    </div>
   );
 }
 
