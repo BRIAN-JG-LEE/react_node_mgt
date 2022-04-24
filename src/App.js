d0b5087e8fe32f93091808eca569a2cce9a4dfb2
JONGGIL LEE <brianlee2k2@gmail.com>
2022-04-24 10:04:48 +0900

    6. Material UI 이용하여 테이블 디자인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import "./App.css";
 import Customer from "./components/Customer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import TableContainer from "@mui/material/TableContainer";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const customers = [
   {
@@ -30,21 +58,35 @@ const customers = [
 
 function App() {
   return (
-    <div>
-      {customers.map((cus) => {
-        return (
-          <Customer
-            key={cus.id}
-            id={cus.id}
-            image={cus.image}
-            name={cus.name}
-            birthday={cus.birthday}
-            gender={cus.gender}
-            job={cus.job}
-          />
-        );
-      })}
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1440 }} aria-label="customized table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell>회원번호</StyledTableCell>
+            <StyledTableCell>이미지</StyledTableCell>
+            <StyledTableCell>이름</StyledTableCell>
+            <StyledTableCell>생년월일</StyledTableCell>
+            <StyledTableCell>성별</StyledTableCell>
+            <StyledTableCell>직업</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        {customers.map((cus) => {
+          return (
+            <TableBody>
+              <Customer
+                key={cus.id}
+                id={cus.id}
+                image={cus.image}
+                name={cus.name}
+                birthday={cus.birthday}
+                gender={cus.gender}
+                job={cus.job}
+              />
+            </TableBody>
+          );
+        })}
+      </Table>
+    </TableContainer>
   );
 }
 
